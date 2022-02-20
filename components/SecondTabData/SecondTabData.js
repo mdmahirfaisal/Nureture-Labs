a9ad38b76fb5b1c6eb9658a2362e095941629f04
unknown <rjmahir.faisal@gmail.com>
2022-02-20 11:28:21 +0600

    second tab table style chenged

--- components/SecondTabData/SecondTabData.js
+++ components/SecondTabData/SecondTabData.js
@@ -5,14 +5,15 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { Grid } from '@mui/material';
 
 function createData(name, calories, fat, carbs, protein) {
     return { name, calories, fat, carbs, protein };
 }
 
 const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Frozen ', 159, 6.0, 24, 4.0),
+    createData('Ice cream ', 237, 9.0, 37, 4.3),
     createData('Eclair', 262, 16.0, 24, 6.0),
     createData('Cupcake', 305, 3.7, 67, 4.3),
     createData('Gingerbread', 356, 16.0, 49, 3.9),
@@ -24,14 +25,14 @@ export default function SecondTabData() {
             <Table >
                 <TableBody sx={{ backgroundColor: '#242731' }}>
                     {rows.map((row) => (
-                        <TableRow className='grid grid-cols-4'
+                        <Grid container spacing={2}
                             key={row.name}
                             sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                            <TableCell sx={{ color: 'white' }} component="th" scope="row">{row.name}</TableCell>
-                            <TableCell sx={{ color: 'white' }} align="center">{row.calories}</TableCell>
-                            <TableCell sx={{ color: 'white' }} align="center">{row.fat}</TableCell>
-                            <TableCell sx={{ color: 'white' }} align="center">{row.carbs}</TableCell>
-                        </TableRow>
+                            <Grid item xs={3} sx={{ color: 'lightgray', fontWeight: '400' }} component="th" scope="row">{row.name}</Grid>
+                            <Grid item xs={3} sx={{ color: 'lightgray', fontWeight: '400' }} align="center">{row.calories}</Grid>
+                            <Grid item xs={3} sx={{ color: 'lightgray', fontWeight: '400' }} align="center">{row.fat}</Grid>
+                            <Grid item xs={3} sx={{ color: 'lightgray', fontWeight: '400' }} align="center">{row.carbs}</Grid>
+                        </Grid>
                     ))}
                 </TableBody>
             </Table>
